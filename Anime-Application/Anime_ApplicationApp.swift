32869774d5b829c78ad76dac858092fd86dd0c15
circosallc <prestonhildebrandt@gmail.com>
2022-06-25 11:40:34 -0700

    Initial Commit

---
+++ Anime-Application/Anime_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anime_ApplicationApp.swift
+//  Anime-Application
+//
+//  Created by Preston Hildebrandt on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Anime_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
