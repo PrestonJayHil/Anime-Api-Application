da1c7c3cde680dda480bc612be466a328a24d868
circosallc <prestonhildebrandt@gmail.com>
2022-06-25 15:38:21 -0700

    First implementation of main screen

--- Anime-Application/ContentView.swift
+++ Anime-Application/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                
+                Text("Anime Finder")
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 30, alignment: .center)
+                    .padding()
+                
+                NavigationLink (destination: Text("Button"),
+                    label: {
+                       ContinueButton()
+                    }
+                )
+            }
+        }.navigationTitle("Home")
+    }
+}
+
+struct ContinueButton: View {
+    var body: some View {
+        Text("Open").frame(width: 200, height: 50, alignment: .center).background(Color.pink)
+            .foregroundColor(.white).cornerRadius(8)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
