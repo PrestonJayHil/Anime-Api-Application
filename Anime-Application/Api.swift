454f1a6b9b5f19d5527ab5790442c1c0ee7616c3
circosallc <prestonhildebrandt@gmail.com>
2022-06-30 13:16:27 -0700

    added api.swfit to the project

---
+++ Anime-Application/Api.swift
@@ -0,0 +1,8 @@
+//
+//  Api.swift
+//  Anime-Application
+//
+//  Created by Preston Hildebrandt on 6/30/22.
+//
+
+import Foundation
--- Anime-Application/ContentView.swift
+++ Anime-Application/ContentView.swift
@@ -14,20 +14,20 @@ struct ContentView: View {
 
                 ZStack {
                     Image("wallpaper").resizable().aspectRatio(contentMode: .fill).frame(width: 450, height: 300, alignment: .center)
-                    // Text("Anime Finder")
-                      //  .aspectRatio(contentMode: .fit)
+                        // Text("Anime Finder")
+                        //  .aspectRatio(contentMode: .fit)
                        .frame(width: 100, height: 200)
-                        .padding()
+                       .padding()
+                    
                     Image("anime").resizable().scaledToFit().offset(x: 0.0, y: -100.0)
                     
-                    
+                       
                     NavigationLink (destination: Text("Button"),
                         label: {
                         OpenButton()
                         }
                     )
                 }
-        
         }.navigationTitle("Home")
     }
 }
@@ -35,7 +35,7 @@ struct ContentView: View {
 struct OpenButton: View {
     var body: some View {
         Text("Open App").frame(width: 200, height: 50, alignment: .center).background(Color.pink)
-            .foregroundColor(.white).cornerRadius(8).navigationBarBackButtonHidden(true)
+            .foregroundColor(.white).cornerRadius(8)
     }
 }
 
